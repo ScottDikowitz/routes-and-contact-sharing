aba7c821d9174a433b87cf1e4ee9a1eb7b527a47
ScottJimmy <ScottJimmy.email>
2015-10-19 12:13:41 -0400

    Add my_script and UsersController#index

---
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+
+  def index
+    render text: "I'm in the index action!"
+  end
+end
---
+++ FirstRoutes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstRoutes::Application.config.secret_key_base = 'e9a7d9e0e8c20d76c87f7a03a45ebb6949af7192bdf86609dced568d24fdb549679e303e036aa7e1f990e17551d5b2c8e2e839fe3aed290d87a0a2c2a613e30b'
